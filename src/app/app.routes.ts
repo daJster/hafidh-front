1ab52385f5c3a3fc8bd1394f38eecec0509269ad
EL KARCHI Jad <jad.el-karchi@inria.fr>
2025-04-21 21:45:29 +0200

    added Auth V1 & V2, npm build scripts functionning, auth, home route functionning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { InjectionToken } from '@angular/core';
 export const APP_CONFIG = new InjectionToken<string>('./app.config');
 
 export const appRoutes: Routes = [
-    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+    { path: 'auth/v1', loadChildren: () => import('./auth/v1/auth.module').then(m => m.AuthModule) },
+    { path: 'auth/v2', loadChildren: () => import('./auth/v2/auth.module').then(m => m.AuthModule) },
     { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
